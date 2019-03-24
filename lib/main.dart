6cd4606d91954c54cdb5d22e49e2b3cfde9a43fa
Anthony Morris <anthonymorris13@gmail.com>
2019-03-24 23:53:27 +1100

    Add firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/foundation.dart' show defaultTargetPlatform;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
@@ -12,9 +14,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FirebaseAnalytics analytics = FirebaseAnalytics();
+
     return MaterialApp(
       title: '500 Scorer',
       home: HomeScreen(),
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
       theme: new ThemeData(
         primarySwatch: Colors.indigo,
         typography: Typography(
